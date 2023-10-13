0b86da99c27e372783742ca50a139a102b067461
Nemodruk Andrey <nemodruk.briz@gmail.com>
2023-10-13 17:16:00 +0300

    HW_4

--- index.js
+++ index.js
@@ -1,270 +1,58 @@
 "use strict";
-var DirectionNames;
-(function (DirectionNames) {
-    DirectionNames["humanitarian"] = "humanitarian";
-    DirectionNames["linguistic"] = "linguistic";
-    DirectionNames["economic"] = "economic";
-})(DirectionNames || (DirectionNames = {}));
-var Levels;
-(function (Levels) {
-    Levels["first"] = "first";
-    Levels["second"] = "second";
-    Levels["third"] = "third";
-    Levels["fourth"] = "fourth";
-    Levels["fifth"] = "fifth";
-})(Levels || (Levels = {}));
-var Attendance;
-(function (Attendance) {
-    Attendance["present"] = "present";
-    Attendance["absent"] = "absent";
-})(Attendance || (Attendance = {}));
-var Subjects;
-(function (Subjects) {
-    Subjects["biology"] = "Biology";
-    Subjects["drawing"] = "Drawing";
-    Subjects["geography"] = "Geography";
-    Subjects["history"] = "History";
-    Subjects["literature"] = "Literature";
-})(Subjects || (Subjects = {}));
-var Marks;
-(function (Marks) {
-    Marks[Marks["one"] = 1] = "one";
-    Marks[Marks["two"] = 2] = "two";
-    Marks[Marks["three"] = 3] = "three";
-    Marks[Marks["four"] = 4] = "four";
-    Marks[Marks["five"] = 5] = "five";
-})(Marks || (Marks = {}));
-var Status;
-(function (Status) {
-    Status["active"] = "active";
-    Status["inactive"] = "inactive";
-})(Status || (Status = {}));
-class School {
-    constructor() {
-        // implement 'add area' ++
-        // 'remove area' ++
-        // 'add lecturer' ++
-        // 'remove lecturer' ++
-        this._areas = [];
-        this._lecturers = []; // Name, surname, position, company, experience, courses, contacts
+var Formulas;
+(function (Formulas) {
+    Formulas["Rectangle"] = "Rectangle";
+    Formulas["Square"] = "Square";
+    Formulas["Circle"] = "Circle";
+    Formulas["Triangle"] = "Triangle";
+})(Formulas || (Formulas = {}));
+class AbstractFigureWithPrint {
+    constructor(name, color, formula) {
+        this.name = name;
+        this.color = color;
+        this.formula = formula;
     }
-    get areas() {
-        return this._areas;
-    }
-    get lecturers() {
-        return this._lecturers;
-    }
-    addArea(area) {
-        this._areas.push(area);
-    }
-    removeArea(id) {
-        this._areas = [...this._areas].filter((i) => i.id !== id);
-    }
-    addLecturer(lecturer) {
-        this._lecturers.push(lecturer);
-    }
-    removeLecturer(id) {
-        this._lecturers = this._lecturers.filter((i) => i.id !== id);
+    print() {
+        console.log(this.formula);
     }
 }
-class Area {
-    constructor({ name, id }) {
-        // implement getters for fields ++
-        // 'add/remove level' methods ++
-        this._levels = [];
-        this._name = name;
-        this._id = id;
+class Rectangle extends AbstractFigureWithPrint {
+    constructor(name, color) {
+        super(name, color, Formulas.Rectangle);
     }
-    get id() {
-        return this._id;
-    }
-    get levels() {
-        return this._levels;
-    }
-    get name() {
-        return this._name;
-    }
-    addLevel(level) {
-        this._levels.push(level);
-    }
-    removeLevel(id) {
-        this._levels = this._levels.filter((i) => i.id !== id);
+    calculateArea(sideA, sideB) {
+        return sideA * sideB;
     }
 }
-class Level {
-    constructor({ name, description, id }) {
-        this._groups = [];
-        this._name = name;
-        this._description = description;
-        this._id = id;
+class Square extends AbstractFigureWithPrint {
+    constructor(name, color) {
+        super(name, color, Formulas.Square);
     }
-    get groups() {
-        return this._groups;
-    }
-    get name() {
-        return this._name;
-    }
-    get description() {
-        return this._description;
-    }
-    get id() {
-        return this._id;
-    }
-    addGroup(group) {
-        this._groups.push(group);
-    }
-    removeGroup(id) {
-        this._groups = this._groups.filter((i) => i.id !== id);
+    calculateArea(sideA, sideB) {
+        return sideA * sideB;
     }
 }
-class Group {
-    constructor({ directionName, levelName, id }) {
-        this._status = Status.inactive;
-        this._students = new StudentsArray(); // Modify the array so that it has a valid toSorted method*
-        this._directionName = directionName;
-        this._levelName = levelName;
-        this._id = id;
+class Circle {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
     }
-    showPerformance() {
-        const sortedStudents = this._students.toSorted((a, b) => b.getPerformanceRating() - a.getPerformanceRating());
-        return sortedStudents;
+    calculateArea(radius) {
+        return Math.PI * Math.pow(radius, 2);
     }
-    get directionName() {
-        return this._directionName;
+}
+class Triangle {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
     }
-    get levelName() {
-        return this._levelName;
-    }
-    get id() {
-        return this._id;
-    }
-    get status() {
-        return this._status;
-    }
-    get students() {
-        return this._students;
-    }
-    setStatusActive() {
-        this._status = Status.active;
-    }
-    setStatusInActive() {
-        this._status = Status.inactive;
-    }
-    addStudent(student) {
-        this._students.push(student);
-    }
-    removeStudent(id) {
-        const index = this._students.findIndex((i) => i.id === id);
-        if (index !== -1) {
-            this._students.splice(index, 1);
+    calculateArea(baseSideOrSideA, heightOrSideB, angle) {
+        if (!angle) {
+            return (baseSideOrSideA * heightOrSideB) / 2;
         }
+        return (baseSideOrSideA * heightOrSideB * Math.sin(angle)) / 2;
     }
 }
-class Student {
-    constructor({ firstName, lastName, birthYear, id }) {
-        this._grades = {
-            Biology: [],
-            Drawing: [],
-            Geography: [],
-            History: [],
-            Literature: [],
-        };
-        this._visits = {
-            Biology: [],
-            Drawing: [],
-            Geography: [],
-            History: [],
-            Literature: [],
-        }; // lesson: present
-        this._firstName = firstName;
-        this._lastName = lastName;
-        this._birthYear = birthYear;
-        this._id = id;
-    }
-    get fullName() {
-        return `${this._lastName} ${this._firstName}`;
-    }
-    set fullName(value) {
-        [this._lastName, this._firstName] = value.split(" ");
-    }
-    get age() {
-        return new Date().getFullYear() - this._birthYear;
-    }
-    get id() {
-        return this._id;
-    }
-    setGrade(subject, mark) {
-        this._grades[subject].push(mark);
-    }
-    setVisits(subject, visit) {
-        this._visits[subject].push(visit);
-    }
-    getPerformanceRating() {
-        const gradeValues = Object.values(this._grades).flat();
-        const visitsValues = Object.values(this._visits).flat();
-        if (!gradeValues.length)
-            return 0;
-        const averageGrade = gradeValues.reduce((sum, grade) => sum + grade, 0) / gradeValues.length;
-        const attendancePercentage = (visitsValues.filter((i) => i === Attendance.present).length /
-            visitsValues.length) *
-            100;
-        return (averageGrade + attendancePercentage) / 2;
-    }
-}
-class StudentsArray extends Array {
-    constructor(...students) {
-        super();
-        this.push(...students);
-    }
-    toSorted(callback) {
-        for (let j = 0; j < this.length; j++) {
-            let isSorted = true;
-            for (let i = 1; i < this.length - j; i++) {
-                if (callback(this[i], this[i - 1]) < 0) {
-                    [this[i], this[i - 1]] = [this[i - 1], this[i]];
-                    isSorted = false;
-                }
-            }
-            if (isSorted)
-                break;
-        }
-        return this;
-    }
-}
-const vasya = new Student({
-    firstName: "Вася",
-    lastName: "Пупкин",
-    birthYear: 2000,
-    id: "0",
-});
-const kostya = new Student({
-    firstName: "Kocтя",
-    lastName: "Сидоров",
-    birthYear: 2000,
-    id: "1",
-});
-const tolik = new Student({
-    firstName: "Толик",
-    lastName: "Иванов",
-    birthYear: 2000,
-    id: "2",
-});
-vasya.setVisits(Subjects.biology, Attendance.absent);
-vasya.setVisits(Subjects.drawing, Attendance.present);
-kostya.setVisits(Subjects.biology, Attendance.absent);
-kostya.setVisits(Subjects.drawing, Attendance.absent);
-tolik.setVisits(Subjects.biology, Attendance.present);
-tolik.setVisits(Subjects.drawing, Attendance.present);
-vasya.setGrade(Subjects.biology, Marks.four);
-vasya.setGrade(Subjects.geography, Marks.four);
-tolik.setGrade(Subjects.biology, Marks.five);
-tolik.setGrade(Subjects.geography, Marks.five);
-const group = new Group({
-    directionName: DirectionNames.economic,
-    levelName: Levels.first,
-    id: "11",
-});
-group.addStudent(vasya);
-group.addStudent(tolik);
-group.addStudent(kostya);
-console.log(group.showPerformance());
+const rectangle = new Rectangle("rect", "red");
+console.log(rectangle.calculateArea(10, 10));
+rectangle.print();
--- index.ts
+++ index.ts
@@ -1,380 +1,89 @@
-enum DirectionNames {
-  humanitarian = "humanitarian",
-  linguistic = "linguistic",
-  economic = "economic",
+enum Formulas {
+  Rectangle = "S = a * b",
+  Square = "S = a^2",
+  Circle = "S = π * r^2",
+  Triangle = "S = 1/2(a * h)",
 }
 
-enum Levels {
-  first = "first",
-  second = "second",
-  third = "third",
-  fourth = "fourth",
-  fifth = "fifth",
+interface IRegtangleData {
+  sideA: number;
+  sideB: number;
+}
+interface ICircleData {
+  radius: number;
 }
 
-enum Attendance {
-  present = "present",
-  absent = "absent",
+interface ISquareData {
+  side: number;
 }
 
-enum Subjects {
-  biology = "Biology",
-  drawing = "Drawing",
-  geography = "Geography",
-  history = "History",
-  literature = "Literature",
+interface ITriangleData {
+  baseSide: number;
+  height: number;
 }
 
-enum Marks {
-  one = 1,
-  two,
-  three,
-  four,
-  five,
+interface IFigureData {
+  regtangleData: IRegtangleData;
+  squareData: ISquareData;
+  circleData: ICircleData;
+  triangleData: ITriangleData;
 }
 
-type Grade = {
-  [key in Subjects]: Marks[];
-};
-
-type Visit = {
-  [key in Subjects]: Attendance[];
-};
-
-type LecturerData = {
-  id: string;
-  Name: string;
-  surname: string;
-  position: string;
-  company: string;
-  experience: string;
-  courses: string;
-  contacts: string;
-};
-
-type AreaData = {
+interface IFigure<T extends keyof IFigureData> {
+  formula: Formulas;
   name: string;
-  id: string;
-};
-
-type LevelData = {
-  id: string;
-  name: Levels;
-  description: string;
-};
-
-type StudentData = {
-  id: string;
-  firstName: string;
-  lastName: string;
-  birthYear: number;
-};
-
-type GroupData = {
-  directionName: DirectionNames;
-  levelName: Levels;
-  id: string;
-};
-
-enum Status {
-  active = "active",
-  inactive = "inactive",
+  color: string;
+  calculateArea: (data: IFigureData[T]) => number;
 }
 
-class School {
-  // implement 'add area' ++
-  // 'remove area' ++
-  // 'add lecturer' ++
-  // 'remove lecturer' ++
-  private _areas: Area[] = [];
-  private _lecturers: LecturerData[] = []; // Name, surname, position, company, experience, courses, contacts
+interface IFigureWithPrint {
+  print: () => void;
+}
 
-  get areas() {
-    return this._areas;
+class Square implements IFigure<"squareData">, IFigureWithPrint {
+  readonly formula: Formulas = Formulas.Square;
+
+  constructor(readonly name: string, readonly color: string) {}
+
+  print() {
+    console.log(this.formula);
   }
 
-  get lecturers() {
-    return this._lecturers;
-  }
-
-  addArea(area: Area) {
-    this._areas.push(area);
-  }
-
-  removeArea(id: string) {
-    this._areas = [...this._areas].filter((i) => i.id !== id);
-  }
-
-  addLecturer(lecturer: LecturerData) {
-    this._lecturers.push(lecturer);
-  }
-
-  removeLecturer(id: string) {
-    this._lecturers = this._lecturers.filter((i) => i.id !== id);
+  calculateArea({ side }: ISquareData) {
+    return Math.pow(side, 2);
   }
 }
 
-class Area {
-  // implement getters for fields ++
-  // 'add/remove level' methods ++
-  private _levels: Level[] = [];
-  private _name: string;
-  private _id: string;
+class Rectangle implements IFigure<"regtangleData">, IFigureWithPrint {
+  readonly formula: Formulas = Formulas.Rectangle;
 
-  constructor({ name, id }: AreaData) {
-    this._name = name;
-    this._id = id;
+  constructor(readonly name: string, readonly color: string) {}
+
+  print() {
+    console.log(this.formula);
   }
 
-  get id() {
-    return this._id;
-  }
-  get levels() {
-    return this._levels;
-  }
-  get name() {
-    return this._name;
-  }
-
-  addLevel(level: Level) {
-    this._levels.push(level);
-  }
-
-  removeLevel(id: string) {
-    this._levels = this._levels.filter((i) => i.id !== id);
+  calculateArea({ sideA, sideB }: IRegtangleData) {
+    return sideA * sideB;
   }
 }
 
-class Level {
-  // implement getters for fields ++
-  // 'add/remove group' methods ++
-  private _id: string;
-  private _groups: Group[] = [];
-  private _name: string;
-  private _description: string;
+class Circle implements IFigure<"circleData"> {
+  readonly formula: Formulas = Formulas.Circle;
 
-  constructor({ name, description, id }: LevelData) {
-    this._name = name;
-    this._description = description;
-    this._id = id;
-  }
+  constructor(readonly name: string, readonly color: string) {}
 
-  get groups() {
-    return this._groups;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  get description() {
-    return this._description;
-  }
-
-  get id() {
-    return this._id;
-  }
-
-  addGroup(group: Group) {
-    this._groups.push(group);
-  }
-
-  removeGroup(id: string) {
-    this._groups = this._groups.filter((i) => i.id !== id);
+  calculateArea({ radius }: ICircleData) {
+    return Math.PI * Math.pow(radius, 2);
   }
 }
 
-class Group {
-  // implement getters for fields ++
-  // and 'add/remove student' ++
-  // and 'set status' methods ++
-  private _directionName: DirectionNames;
-  private _levelName: Levels;
-  private _id: string;
-  private _status: Status = Status.inactive;
-  private _students = new StudentsArray(); // Modify the array so that it has a valid toSorted method*
+class Triangle implements IFigure<"triangleData"> {
+  readonly formula: Formulas = Formulas.Triangle;
 
-  constructor({ directionName, levelName, id }: GroupData) {
-    this._directionName = directionName;
-    this._levelName = levelName;
-    this._id = id;
-  }
+  constructor(readonly name: string, readonly color: string) {}
 
-  showPerformance() {
-    const sortedStudents = this._students.toSorted(
-      (a, b) => b.getPerformanceRating() - a.getPerformanceRating()
-    );
-    return sortedStudents;
-  }
-
-  get directionName() {
-    return this._directionName;
-  }
-  get levelName() {
-    return this._levelName;
-  }
-  get id() {
-    return this._id;
-  }
-
-  get status() {
-    return this._status;
-  }
-
-  get students() {
-    return this._students;
-  }
-
-  setStatusActive() {
-    this._status = Status.active;
-  }
-
-  setStatusInActive() {
-    this._status = Status.inactive;
-  }
-
-  addStudent(student: Student) {
-    this._students.push(student);
-  }
-
-  removeStudent(id: string) {
-    const index = this._students.findIndex((i) => i.id === id);
-    if (index !== -1) {
-      this._students.splice(index, 1);
-    }
+  calculateArea({ height, baseSide }: ITriangleData): number {
+    return (height * baseSide) / 2;
   }
 }
-
-class Student {
-  // implement 'set grade' ++
-  // 'set visit' methods ++
-  private _id: string;
-  private _firstName: string;
-  private _lastName: string;
-  private _birthYear: number;
-  private _grades: Grade = {
-    Biology: [],
-    Drawing: [],
-    Geography: [],
-    History: [],
-    Literature: [],
-  };
-  private _visits: Visit = {
-    Biology: [],
-    Drawing: [],
-    Geography: [],
-    History: [],
-    Literature: [],
-  }; // lesson: present
-
-  constructor({ firstName, lastName, birthYear, id }: StudentData) {
-    this._firstName = firstName;
-    this._lastName = lastName;
-    this._birthYear = birthYear;
-    this._id = id;
-  }
-
-  get fullName() {
-    return `${this._lastName} ${this._firstName}`;
-  }
-
-  set fullName(value) {
-    [this._lastName, this._firstName] = value.split(" ");
-  }
-
-  get age() {
-    return new Date().getFullYear() - this._birthYear;
-  }
-
-  get id() {
-    return this._id;
-  }
-
-  setGrade(subject: Subjects, mark: Marks) {
-    this._grades[subject].push(mark);
-  }
-
-  setVisits(subject: Subjects, visit: Attendance) {
-    this._visits[subject].push(visit);
-  }
-
-  getPerformanceRating() {
-    const gradeValues = Object.values(this._grades).flat();
-    const visitsValues = Object.values(this._visits).flat();
-
-    if (!gradeValues.length) return 0;
-
-    const averageGrade =
-      gradeValues.reduce((sum, grade) => sum + grade, 0) / gradeValues.length;
-    const attendancePercentage =
-      (visitsValues.filter((i) => i === Attendance.present).length /
-        visitsValues.length) *
-      100;
-
-    return (averageGrade + attendancePercentage) / 2;
-  }
-}
-
-class StudentsArray extends Array<Student> {
-  constructor(...students: Student[]) {
-    super();
-    this.push(...students);
-  }
-
-  public toSorted(callback: (a: Student, b: Student) => number) {
-    for (let j = 0; j < this.length; j++) {
-      let isSorted = true;
-      for (let i = 1; i < this.length - j; i++) {
-        if (callback(this[i], this[i - 1]) < 0) {
-          [this[i], this[i - 1]] = [this[i - 1], this[i]];
-          isSorted = false;
-        }
-      }
-      if (isSorted) break;
-    }
-    return this;
-  }
-}
-
-const vasya = new Student({
-  firstName: "Вася",
-  lastName: "Пупкин",
-  birthYear: 2000,
-  id: "0",
-});
-const kostya = new Student({
-  firstName: "Kocтя",
-  lastName: "Сидоров",
-  birthYear: 2000,
-  id: "1",
-});
-const tolik = new Student({
-  firstName: "Толик",
-  lastName: "Иванов",
-  birthYear: 2000,
-  id: "2",
-});
-
-vasya.setVisits(Subjects.biology, Attendance.absent);
-vasya.setVisits(Subjects.drawing, Attendance.present);
-kostya.setVisits(Subjects.biology, Attendance.absent);
-kostya.setVisits(Subjects.drawing, Attendance.absent);
-tolik.setVisits(Subjects.biology, Attendance.present);
-tolik.setVisits(Subjects.drawing, Attendance.present);
-
-vasya.setGrade(Subjects.biology, Marks.four);
-vasya.setGrade(Subjects.geography, Marks.four);
-tolik.setGrade(Subjects.biology, Marks.five);
-tolik.setGrade(Subjects.geography, Marks.five);
-
-const group = new Group({
-  directionName: DirectionNames.economic,
-  levelName: Levels.first,
-  id: "11",
-});
-
-group.addStudent(vasya);
-group.addStudent(tolik);
-group.addStudent(kostya);
-
-console.log(group.showPerformance());
